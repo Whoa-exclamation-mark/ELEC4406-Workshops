459a1d597e4be16d17ca81489242c0f69be4a920
Campbell Millar <2208334+Whoa-exclamation-mark@users.noreply.github.com>
2021-09-28 12:48:22 +0800

    Fixed a mistake in the FSM example component

--- components/blank_fsm.vhd
+++ components/blank_fsm.vhd
@@ -9,7 +9,7 @@ entity blank_fsm is
 			RESET: IN STD_LOGIC;
 			Z1, Z2 : IN STD_LOGIC;
 			Y1, Y2: OUT STD_LOGIC;
-			state_out : out STD_LOGIC(1 downto 0);
+			state_out : out STD_LOGIC(1 downto 0)
        );
 end blank_entity;
 	
